d0532664e47b6ebac9ff60dfcc9124c24587ab3f
AmiasYaska <118506048+AmiasYaska@users.noreply.github.com>
2024-06-24 17:08:38 +0300

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./public/*.html"],
   theme: {
     extend: {},
+    fontFamily: {
+      Poppins: ["Poppins", "sans-serif"],
+    },
     container: {
       center: true,
       padding: '1rem',
