db4ded5f6e34e021c9ba5e6df9d6c563aefc0a75
AmiasYaska <118506048+AmiasYaska@users.noreply.github.com>
2024-06-24 17:14:34 +0300

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./public/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "page-purple": "#5267DF",
+        "page-red": "#FA5959",
+        "page-blue": "#242A45",
+        "page-gray": "#9194A2",
+        "page-white": "#f7f7f7",
+      }
+    },
     fontFamily: {
       Poppins: ["Poppins", "sans-serif"],
     },
