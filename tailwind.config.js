a9b326192c6fa2300b75cf1f18d2fc7e0df6ef39
AmiasYaska <118506048+AmiasYaska@users.noreply.github.com>
2024-06-24 17:01:47 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+      padding: '1rem',
+    },
+  },
+  plugins: [],
+}
+
